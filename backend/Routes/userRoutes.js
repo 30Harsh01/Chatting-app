861d940568177021fe14991341d84c00befe14f5
Harsh saxena <harshsaxena427@gmail.com>
2024-04-12 20:27:25 +0530

    User Routes completed

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,9 +1,20 @@
 import express from "express"
+import protectRoute from "../Middleware/protectRoute.js"
+import User from "../Model/userSchema.js"
 
 const router=express.Router()
 
-router.get("/user",(req,res)=>{
-    res.send('hey')
+//get users for sidebar
+router.get("/",protectRoute,async(req,res)=>{
+    try {
+        const loggedInUser=req.user._id
+        const alluser=await User.find({_id:{$ne:loggedInUser}}).select("-password")
+
+        res.status(200).json(alluser)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Internal server error from userroute"})
+    }
 })
 
 
